1a2e0c87e6399b0089d5cfcd598c07e842498fe4
fabio-santana <exconcs@gmail.com>
2021-04-10 16:41:08 -0300

    garantir que o RemoteAuthentication chame HttpClient com o metodo correto

--- test/data/usecases/remoteAuthentication_test.dart
+++ test/data/usecases/remoteAuthentication_test.dart
@@ -1,6 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -12,20 +13,21 @@ class RemoteAuthentication {
   });
 
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
   Future<void> request({
-    String url,
+    @required String url,
+    @required String method,
   });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Deve chamar HttpClient com a URL correta', () async {
+  test('Deve chamar HttpClient com valores corretos', () async {
     //sut = system under test
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
@@ -33,6 +35,9 @@ void main() {
 
     await sut.auth();
 
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+    ));
   });
 }
